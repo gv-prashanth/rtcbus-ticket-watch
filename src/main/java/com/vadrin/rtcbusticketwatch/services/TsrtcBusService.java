0fb7d921d16836236bbf94540f7d7bdf2cbe614d
Gumma <3628612@lowes.com>
2023-08-29 22:17:35 +0530

    fixing tsrtc header issue

--- src/main/java/com/vadrin/rtcbusticketwatch/services/TsrtcBusService.java
+++ src/main/java/com/vadrin/rtcbusticketwatch/services/TsrtcBusService.java
@@ -2,10 +2,14 @@ package com.vadrin.rtcbusticketwatch.services;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
 import com.vadrin.rtcbusticketwatch.models.ExceptionWhileFetchingFromCorporation;
 import com.vadrin.rtcbusticketwatch.models.Request;
 
@@ -18,10 +22,15 @@ public class TsrtcBusService implements BusService {
   @Override
   public int getNumberOfAvailabeSeats(Request request) throws ExceptionWhileFetchingFromCorporation {
     RestTemplate restTemplate = restTemplateBuilder.build();
+    //Use Below user-agent
+    //Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36
+    HttpHeaders headers = new HttpHeaders();
+    headers.set("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36");      
+    HttpEntity hpptEntity = new HttpEntity<>(null, headers);
     String response = restTemplate
         .postForObject("https://www.tsrtconline.in/oprs-web/forward/booking/avail/services.do?txtJourneyDate="
             + request.getJourneyDate() + "&startPlaceId=" + request.getStartPlaceId() + "&endPlaceId="
-            + request.getEndPlaceId(), null, String.class);
+            + request.getEndPlaceId(), hpptEntity, String.class);
     Pattern pattern = Pattern.compile("fwTotalSeats\" value=\"(.*?)\"");
     Matcher matcher = pattern.matcher(response);
     if (matcher.find()) {
