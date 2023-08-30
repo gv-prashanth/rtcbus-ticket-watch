92e45ae16e2946ef2cf6148bdc5cc1db3022b823
Gumma <3628612@lowes.com>
2023-08-30 11:14:34 +0530

    adding same code as aps

--- src/main/java/com/vadrin/rtcbusticketwatch/services/TsrtcBusService.java
+++ src/main/java/com/vadrin/rtcbusticketwatch/services/TsrtcBusService.java
@@ -1,5 +1,6 @@
 package com.vadrin.rtcbusticketwatch.services;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -7,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -22,17 +26,25 @@ public class TsrtcBusService implements BusService {
   @Override
   public int getNumberOfAvailabeSeats(Request request) throws ExceptionWhileFetchingFromCorporation {
     RestTemplate restTemplate = restTemplateBuilder.build();
-    //Use Below user-agent
-    //Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36
     HttpHeaders headers = new HttpHeaders();
-    headers.set("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36");      
-    HttpEntity hpptEntity = new HttpEntity<>(null, headers);
-    String response = restTemplate
-        .postForObject("https://www.tsrtconline.in/oprs-web/forward/booking/avail/services.do?txtJourneyDate="
+    headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+    headers.setContentType(MediaType.APPLICATION_JSON);
+    headers.set("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36");
+    headers.set("host", "https://www.tsrtconline.in");
+    headers.set("origin", "https://www.tsrtconline.in");
+    headers.set("referer", "https://www.tsrtconline.in/oprs-web/avail/services.do");
+    headers.set("x-requested-with", "XMLHttpRequest");
+    HttpEntity<String> entity = new HttpEntity<>(headers);
+//    String response = restTemplate
+//        .postForObject("https://www.tsrtconline.in/oprs-web/forward/booking/avail/services.do?txtJourneyDate="
+//            + request.getJourneyDate() + "&startPlaceId=" + request.getStartPlaceId() + "&endPlaceId="
+//            + request.getEndPlaceId(), hpptEntity, String.class);
+    ResponseEntity<String> response = restTemplate
+        .exchange("https://www.tsrtconline.in/oprs-web/forward/booking/avail/services.do?txtJourneyDate="
             + request.getJourneyDate() + "&startPlaceId=" + request.getStartPlaceId() + "&endPlaceId="
-            + request.getEndPlaceId(), hpptEntity, String.class);
+            + request.getEndPlaceId(), HttpMethod.POST, entity, String.class);
     Pattern pattern = Pattern.compile("fwTotalSeats\" value=\"(.*?)\"");
-    Matcher matcher = pattern.matcher(response);
+    Matcher matcher = pattern.matcher(response.getBody());
     if (matcher.find()) {
       return Integer.valueOf(matcher.group(1));
     }
